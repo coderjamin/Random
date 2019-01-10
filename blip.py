e5371d532f9e3cf9af6ea22f927a923b90e03c86
Jamin Chowdhury <46355702+coderjamin@users.noreply.github.com>
2019-01-10 17:04:57 -0500

    Add files via upload

---
+++ blip.py
@@ -0,0 +1,60 @@
+print("Hey my name is Jamin \nHow are you today?")
+print("""
+       I'm doing okay, \nAnd how are you today??
+
+       """)
+print( """
+I am doing everything in my power to understand this.
+But it is indeed very confusing.
+""")
+
+print( "Y" + 3 * "ooo")
+print( "Y" + 5 * "ooo" + "??")
+print( "Y" + 3 * "ooo" + 5 * "!!")
+
+print("Wh" "at do you want?")
+
+print( "To be really honest"
+       ", I don't even know anymore.")
+
+word = "wassup"
+print( word[1] + 2* word[2] )
+print ( word[1] + 4* word[2] + 2* word[3] + "??!" ) 
+print ( "Yes, " + word[1] + 2* word[2] + 3* "!!!" )
+
+x = "Independently"
+print( x[3] + x[5] + x[8] + x[-5] + x[-8] + x[-10] )
+print( 3* x[3] + x[5] + x[8] + x[-5] + x[-8] + x[-10] + "??" ) 
+print( x[-1] + 8* x[3] + 2* x[-10] + 5* "!!!!!" )
+
+print( word[1:4] + word[3] + " and titties" )
+print( word[:2] + word [2:] + " widdit??" )
+
+y = "box"
+print( y[1:] )
+print( "T" + y[1:])
+print( "L" + y[1:])
+print( "R" + y[1:])
+print( "Kn" + y[1:])
+print( "D" + y[1:])
+print( "P" + y[1:])
+print( "W" + y[1:])
+print( "S" + y[1:])
+print( "B" + y[1:])
+
+z = "Kazi"
+print ( "B" + z[1:] )
+print ( "N" + z[1:] )
+print ( "Sh" + z[1:] )
+print ( "L" + z[1:] )
+print ( "Cr" + z[1:] )
+print ( "Z" + z[1:] )
+
+print(len(x) + len(y) + len(z))
+print(len(x))
+print(len(y))
+print(len(z))
+
+
+
+
